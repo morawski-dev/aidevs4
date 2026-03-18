951768f30785fbd43841b1e865aa3f788beeacb0
Mateusz Morawski <35307739+morawski-dev@users.noreply.github.com>
2026-03-18 18:15:00 +0200

    project init

---
+++ src/main/java/com/morawski/dev/aidevs/Aidevs4Application.java
@@ -0,0 +1,13 @@
+package com.morawski.dev.aidevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aidevs4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aidevs4Application.class, args);
+	}
+
+}
