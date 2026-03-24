c4ff69eec33fcadbab4326d82812cddf053e633a
Mateusz Morawski <35307739+morawski-dev@users.noreply.github.com>
2026-03-24 17:33:00 +0100

    update Aidevs4Application.java

--- src/main/java/com/morawski/dev/aidevs/Aidevs4Application.java
+++ src/main/java/com/morawski/dev/aidevs/Aidevs4Application.java
@@ -1,13 +1,19 @@
 package com.morawski.dev.aidevs;
 
+import me.paulschwarz.springdotenv.spring.DotenvApplicationInitializer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Aidevs4Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Aidevs4Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        var app = new SpringApplication(Aidevs4Application.class);
+        app.addInitializers(new DotenvApplicationInitializer());
+        app.setWebApplicationType(WebApplicationType.NONE);
+        app.run(args);
+    }
 }
